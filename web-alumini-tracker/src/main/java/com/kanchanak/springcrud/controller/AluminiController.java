1a1b8388294f07ba88a8f5f6fd62fb2b0ce8644d
Kanchanak <kanchanak@Kanchanaks-MacBook-Pro.local>
2021-05-26 13:43:30 -0700

    more modifications on list-alumini.jsp and AluminiController.java

--- web-alumini-tracker/src/main/java/com/kanchanak/springcrud/controller/AluminiController.java
+++ web-alumini-tracker/src/main/java/com/kanchanak/springcrud/controller/AluminiController.java
@@ -1,15 +1,31 @@
 package com.kanchanak.springcrud.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kanchanak.springcrud.dao.AluminusDAO;
+import com.kanchanak.springcrud.entity.Aluminus;
 
 @Controller
 @RequestMapping("/alumini")
 public class AluminiController {
 
+	//need to inject the alumini dao
+	@Autowired
+	private AluminusDAO aluminusDAO;
+	
+	
 	@RequestMapping("/list")
 	public String listAlumini(Model theModel) {
+		
+		//get alumini from the dao
+		List<Aluminus> theAlumini = aluminusDAO.getAlumini();
+		//add the alumini to the model  
+		theModel.addAttribute("alumini", theAlumini);
 		
 		return "list-alumini";
 	}
--- web-alumini-tracker/src/main/webapp/WEB-INF/view/list-alumini.jsp
+++ web-alumini-tracker/src/main/webapp/WEB-INF/view/list-alumini.jsp
@@ -1,3 +1,5 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
 <!DOCTYPE html>
 
 <html>
@@ -8,7 +10,43 @@
 </head>
 
 <body>
-
+	
+	<div id="wrapper">
+	
+		<div id="header">
+			<h2>ARM - Alumini Relationship Manager</h2>
+		
+		</div>
+	
+	
+	</div>
+	
+	<div id = "container">
+		<div id="content">
+			<!--  add our html table here -->
+			<table>
+				<tr>
+					<th>First Name</th>
+					<th>Last Name<th>
+					<th>Email<th>
+					
+				
+				</tr>
+				
+				<!--  loop over and print our alumini -->
+				<c:forEach var="tempAlumini" items="${alumini}">
+					<tr>
+					<td> ${tempAlumini.firstName }</td>
+					<td> ${tempAlumini.lastName }</td>
+					<td> ${tempAlumini.email} </td>
+					
+					</tr>
+				</c:forEach>			
+			</table>
+		</div>
+	
+	
+	</div>
 List Alumini - coming soon ...
 
 </body>
